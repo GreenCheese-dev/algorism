474a810f21f2886f421b98c7fb97e779ce899de7
greenCheese <greencheese.dev@gmail.com>
2023-02-04 18:40:20 +0900

    SOL: javascript

--- js/230131-1.js
+++ js/230131-1.js
@@ -1,3 +1,6 @@
+// programers
+// https://school.programmers.co.kr/learn/courses/30/lessons/150370?language=javascript
+
 // input terms
 // return 약관에 따른 유효 날짜 map
 function getTermsMap(terms) {
@@ -8,39 +11,56 @@ function getTermsMap(terms) {
   return termMap;
 }
 
-function getDayMap(yyyymmdd) {
+function getDayObj(yyyymmdd) {
   var dayMap = {
     YYYY: Number(yyyymmdd.slice(0, 4)),
     MM: Number(yyyymmdd.slice(5, 7)),
-    DD: Number(yyyymmdd.slice(9, 11)),
+    DD: Number(yyyymmdd.slice(8, 11)),
   };
   return dayMap;
 }
 
-// input today, term, privacie
+// input term, privacie
 // return 폐기일
-function isDelete(todayMap, term, privacy) {
-  console.log(todayMap, term, privacy);
+function getDelDate(term, privacy) {
   var delMonth = term + privacy.MM;
-  if (delMonth <= 12) {
+  if (delMonth > 12) {
+    let addYear = ~~(delMonth / 12);
+    privacy.YYYY += addYear;
+    delMonth %= 12;
+    privacy.MM = delMonth;
+  } else {
+    privacy.MM = delMonth;
   }
+  return privacy;
+}
+
+function transDay(map) {
+  day = map.YYYY * 12 * 28 + map.MM * 28 + map.DD;
+  return day;
+}
+// input today, term, privacie
+// return bool 삭제: ture
+function isDelete(todayMap, term, privacy) {
+  privacy = getDelDate(term, privacy);
+  return transDay(todayMap) >= transDay(privacy) ? true : false;
 }
 
 function solution(today, terms, privacies) {
-  var todayMap = getDayMap(today);
+  var todayMap = getDayObj(today);
   var termMap = getTermsMap(terms);
   var answer = [];
 
   for (let i = 0; i < privacies.length; i++) {
-    if (isDelete(todayMap, Number(termMap.get(privacies[i].slice(-1))), getDayMap(privacies[i].slice(0, 10)))) {
+    if (isDelete(todayMap, Number(termMap.get(privacies[i].slice(-1))), getDayObj(privacies[i].slice(0, 10)))) {
+      answer.push(i + 1);
     }
   }
-
   return answer;
 }
 
-var today = '2022.05.19';
-var terms = ['A 6', 'B 12', 'C 3'];
-var privacies = ['2021.05.02 A', '2021.07.01 B', '2022.02.19 C', '2022.02.20 C'];
+var today = '2020.01.01';
+var terms = ['Z 3', 'D 5'];
+var privacies = ['2019.01.01 D', '2019.11.15 Z', '2019.08.02 D', '2019.07.01 D', '2018.12.28 Z'];
 
-solution(today, terms, privacies);
+console.log(solution(today, terms, privacies));
